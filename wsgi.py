0420c896e373859b3b5938590676c690fb8a1db6
Hào Nghiêm <qtvhao@gmail.com>
2023-02-05 21:40:40 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,26 @@
 from flask import Flask, request, render_template
+from transformers import AutoTokenizer, AutoModel
 
 app = Flask(__name__)
+
+MODEL_NAME = "Helsinki-NLP/opus-mt-en-vi"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModel.from_pretrained(MODEL_NAME)
+
+def translate(direction, text):
+    input_ids = tokenizer.encode(text, return_tensors="pt").to("cpu").tolist()[0]
+    output = model.generate(input_ids)
+    translated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return translated_text
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         direction = request.form.get("direction")
         text = request.form.get("text")
-        translated_text = translate(direction, text) # replace this line with your own translation logic
+        translated_text = translate(direction, text)
         return render_template("index.html", text=translated_text)
     return render_template("index.html")
-
-def translate(direction, text):
-    # Your translation logic goes here, use the "direction" parameter to determine the direction of translation
-    return translated_text
 
 if __name__ == "__main__":
     app.run(debug=True)
