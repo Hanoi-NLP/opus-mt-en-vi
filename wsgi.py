575757044a7aca05f48bb9e855382a817ada2a7b
Hào Nghiêm <qtvhao@gmail.com>
2023-02-05 21:37:58 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        direction = request.form.get("direction")
         text = request.form.get("text")
-        translated_text = translate(text) # replace this line with your own translation logic
+        translated_text = translate(direction, text) # replace this line with your own translation logic
         return render_template("index.html", text=translated_text)
     return render_template("index.html")
 
-def translate(text):
-    # Your translation logic goes here
+def translate(direction, text):
+    # Your translation logic goes here, use the "direction" parameter to determine the direction of translation
     return translated_text
 
 if __name__ == "__main__":
